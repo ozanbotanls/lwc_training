0efbca72bc4b41b13e1f2bc3c1d3ced64a4fd6bc
Ozan Botan <ozan.botan@lightspeedhq.com>
2020-02-03 22:04:26 -0500

    * emp api component to subscribe platform events and  to reflect received messages outside. * home page layout arrangements to place the component inside.

---
+++ force-app/main/default/lwc/empApiCmp/empApiCmp.js
@@ -0,0 +1,53 @@
+import { LightningElement, track } from 'lwc';
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
+import { showSuccessMessage } from "c/showMessageHelper";
+
+export default class EmpApiCmp extends LightningElement {
+    @track channelName = '/event/empApi_Test__e';
+    @track isSubscribeDisabled = false;
+    @track isUnsubscribeDisabled = !this.isSubscribeDisabled;
+
+    subscription = {};
+
+    handleChannelName(event) {
+        this.channelName = event.target.value;
+    }
+
+    handleSubscribe() {
+        // Callback invoked whenever a new event message is received
+        const messageCallback = function(response) {
+            showSuccessMessage('New message received', response.data.payload.Message__c);
+        };
+
+        // Invoke subscribe method of empApi. Pass reference to messageCallback
+        subscribe(this.channelName, -1, messageCallback).then(response => {
+            // Response contains the subscription information on successful subscribe call
+            console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
+            this.subscription = response;
+            this.toggleSubscribeButton(true);
+        });
+    }
+
+    handleUnsubscribe() {
+        this.toggleSubscribeButton(false);
+
+        // Invoke unsubscribe method of empApi
+        unsubscribe(this.subscription, response => {
+            console.log('unsubscribe() response: ', JSON.stringify(response));
+            // Response is true for successful unsubscribe
+        });
+    }
+
+    toggleSubscribeButton(enableSubscribe) {
+        this.isSubscribeDisabled = enableSubscribe;
+        this.isUnsubscribeDisabled = !enableSubscribe;
+    }
+
+    registerErrorListener() {
+        // Invoke onError empApi method
+        onError(error => {
+            console.log('Received error from server: ', JSON.stringify(error));
+            // Error contains the server-side error
+        });
+    }
+}
